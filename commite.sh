8f06da8fa7a26f11281b6ebd74f7ff6cd8475dee
iassenegal221 <iassenegal221@gmail.com>
2022-02-06 12:06:01 +0100

    commit_msg

--- commite.sh
+++ commite.sh
@@ -9,4 +9,4 @@ read commit_msgs
 git add .
 git commit -m commit_msg
 git pull --rebase
-git push --set-upstream https://iassenegal221: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@github.com/iassenegal221/Notebooks-formations-IAS.git main
+git push --set-upstream https://iassenegal221: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@github.com/iassenegal221/Notebooks-formations-IAS.git main
