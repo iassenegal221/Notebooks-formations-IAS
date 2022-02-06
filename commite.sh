35453f0044e58943899368aa895af8f78c20bfe0
iassenegal221 <iassenegal221@gmail.com>
2022-02-06 11:52:47 +0100

    commit_msg

--- commite.sh
+++ commite.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-git config --global user.email "email@gmail.com"
-git config --global user.name "user_name"
+git config --global user.email "iassenegal221@gmail.com"
+git config --global user.name "iassenegal221"
 echo " commit message ?"
 read commit_msgs
 
 git add .
 git commit -m commit_msg
 git pull --rebase
-git push --set-upstream https://GITHUB_USER_NAME:GITHUB_ACESS_TOKEN@github.com/iassenegal221/Notebooks-formations-IAS.git main
+git push --set-upstream https://iassenegal221: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@github.com/iassenegal221/Notebooks-formations-IAS.git main
